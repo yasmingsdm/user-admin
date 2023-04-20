d2235a1618178386db538f7a96af9331abc0e086
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-04-20 14:08:23 +0200

    start to create a session

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 const { signUpUser, VerifyEmail, loginUser, logoutUser } = require('../controllers/users')
+const session = require('express-session')
 const formidable = require('express-formidable')
 
 const router = require('express').Router()
 
+router.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
   router.post('/signup', formidable(), signUpUser)
   router.post('/verify-email', VerifyEmail)
   router.post('/login', loginUser)
