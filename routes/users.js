cc72092756c3ae9fc8d2e3606d5c0f96a6fdd3b5
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-04-24 13:52:28 +0200

    finish session and add middlewares loggedin and loggedout

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,21 @@
 const { signUpUser, VerifyEmail, loginUser, logoutUser } = require('../controllers/users')
 const session = require('express-session')
 const formidable = require('express-formidable')
+const dev = require('../config/config')
+const { loggedin, loggedout } = require('../middlewares/auth')
 
 const router = require('express').Router()
 
 router.use(session({
-  secret: 'keyboard cat',
+  name: 'user session',
+  secret: dev.sessionKey || 'VBELUBVRTVIG5BTR55',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: false, maxAge: 60*6000 } //1h
 }))
   router.post('/signup', formidable(), signUpUser)
   router.post('/verify-email', VerifyEmail)
-  router.post('/login', loginUser)
-  router.get('/logout', logoutUser)
+  router.post('/login', loggedout,  loginUser)
+  router.get('/logout', loggedin, logoutUser)
 
  module.exports = router
